1c76af0641b7209999491ac5aeae5decac367bd3
diegotimao <diegotimao1104@gmail.com>
2023-02-13 10:52:35 -0500

    feat: Implementando a função analyze_log lançando exessões e impementando função auxiliar da cliente maria

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,46 @@
+import csv
+from collections import Counter
+import os
+# Qual o prato mais pedido por 'maria'?
+# Quantas vezes 'arnaldo' pediu 'hamburguer'?
+
+# Quais pratos 'joao' nunca pediu?
+# Quais dias 'joao' nunca foi à lanchonete?
+
+def pratos_maria(content):
+    lista_ped = []
+
+    for item in content:
+        if item["cliente"] == 'maria':
+            lista_ped.append(item["prato"])
+
+    values = Counter(lista_ped).most_common(1)[0][0]
+
+    return values
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    extension = os.path.splitext(path_to_file)[1]
+    
+    if extension == '.csv':
+        try:
+            with open(path_to_file) as file:
+                file_csv = csv.DictReader(file, fieldnames=["cliente", "prato", "dia"])
+                max_pedido_maria = pratos_maria(file_csv)
+
+
+            with open('data/mkt_campaign.txt', 'w') as file:
+                file.write(max_pedido_maria)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Arquivo inexistente: {path_to_file}")
+    else:
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    
+
+    
+
+
+
+
+    
+
